4e3840e124d3e9fc17dac6adeeb02281030a8012
Arilson B. Ferreira <arilsondeveloper@gmail.com>
2024-03-30 22:46:19 -0300

    Add swagger

--- Program.cs
+++ Program.cs
@@ -4,11 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
 
 app.UseRouting();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 // app.MapGet("/", () => "Hello World!");
 
